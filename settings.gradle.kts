ef6d6de89d8cbe0ca7072b8471f5f7d83a74c441
Hiroto Kitamura <kirikonknow@gmail.com>
2025-10-12 06:59:44 +0900

    setup by spring initilizr.

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "vcu.pdate.ruler.sample"
---
+++ src/main/kotlin/jp/eno314/vcu/pdate/ruler/sample/VersionCatalogUpdateRulerSampleApplication.kt
@@ -0,0 +1,11 @@
+package jp.eno314.vcu.pdate.ruler.sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VersionCatalogUpdateRulerSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<VersionCatalogUpdateRulerSampleApplication>(*args)
+}
