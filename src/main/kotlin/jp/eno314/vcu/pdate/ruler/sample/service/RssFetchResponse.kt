29b77b2b00da19e500049db98f53af783bd546fe
Hiroto Kitamura <kirikonknow@gmail.com>
2026-04-29 18:07:25 +0900

    chore: update README and cleanup RssFetchResponse

--- src/main/kotlin/jp/eno314/vcu/pdate/ruler/sample/service/RssFetchResponse.kt
+++ src/main/kotlin/jp/eno314/vcu/pdate/ruler/sample/service/RssFetchResponse.kt
@@ -1,44 +1,39 @@
 package jp.eno314.vcu.pdate.ruler.sample.service
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.OffsetDateTime
 
 data class RssFetchResponse(
-    @field:JsonProperty("site_info")
-    @Schema(description = "Information about the RSS site")
+    @field:Schema(description = "Information about the RSS site")
     val siteInfo: SiteInfo,
-    @Schema(description = "List of RSS items")
+    @field:Schema(description = "List of RSS items")
     val items: List<RssItem>,
 )
 
 data class SiteInfo(
-    @Schema(description = "Title of the site", example = "Example Site")
+    @field:Schema(description = "Title of the site", example = "Example Site")
     val title: String,
-    @Schema(description = "Link to the site", example = "https://example.com")
+    @field:Schema(description = "Link to the site", example = "https://example.com")
     val link: String,
-    @Schema(description = "Description of the site", example = "An example site for RSS")
+    @field:Schema(description = "Description of the site", example = "An example site for RSS")
     val description: String?,
 )
 
 data class RssItem(
-    @Schema(description = "Unique ID of the item", example = "12345")
+    @field:Schema(description = "Unique ID of the item", example = "12345")
     val id: String?,
-    @Schema(description = "Title of the item", example = "Example Article")
+    @field:Schema(description = "Title of the item", example = "Example Article")
     val title: String,
-    @Schema(description = "Link to the item", example = "https://example.com/articles/1")
+    @field:Schema(description = "Link to the item", example = "https://example.com/articles/1")
     val link: String,
-    @field:JsonProperty("summary_html")
-    @Schema(description = "Summary of the item in HTML format", example = "<p>This is an example summary.</p>")
+    @field:Schema(description = "Summary of the item in HTML format", example = "<p>This is an example summary.</p>")
     val summaryHtml: String?,
-    @field:JsonProperty("published_at")
-    @Schema(description = "Publication date and time of the item")
+    @field:Schema(description = "Publication date and time of the item")
     val publishedAt: OffsetDateTime,
-    @Schema(description = "Author of the item", example = "John Doe")
+    @field:Schema(description = "Author of the item", example = "John Doe")
     val author: String?,
-    @field:JsonProperty("thumbnail_url")
-    @Schema(description = "URL of the thumbnail image", example = "https://example.com/thumb.jpg")
+    @field:Schema(description = "URL of the thumbnail image", example = "https://example.com/thumb.jpg")
     val thumbnailUrl: String?,
-    @Schema(description = "Categories associated with the item", example = "[\"Technology\", \"Programming\"]")
+    @field:Schema(description = "Categories associated with the item", example = "[\"Technology\", \"Programming\"]")
     val categories: List<String>,
 )
